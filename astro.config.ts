bd82769c6034239fbcae6129254f4bc513d33823
Camilo Vargas <123763043+camvar01@users.noreply.github.com>
2025-08-05 20:50:32 -0500

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tehctim42.com',
+  site: 'https://camvar01.github.io',
   integrations: [
     react(),
     tailwind({
